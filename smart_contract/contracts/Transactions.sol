15c5456a2fd8cb01f6cfbdae2b6160c753722e06
Your Name <you@example.com>
2022-01-13 23:07:26 +0200

    solidity set up

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions{
+    uint256 traansactionsCounter;
+    event Transfer(address from,address receiver,uint amount,string message,uint256 timestamp,string keywaord );
+    struct TransferStruct{
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+  TransferStruct[]  transactions;
+}
