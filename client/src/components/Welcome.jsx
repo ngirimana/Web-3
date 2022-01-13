ed37839e1eea7b6fc8a6707e6f673094a2ef534a
Your Name <you@example.com>
2022-01-13 22:27:31 +0200

    gitignore

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -20,10 +20,11 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
   
   const connectWallet = () => {};
+  const handleSubmit = () => {};
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10 ">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10 ">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -51,7 +52,7 @@ const Welcome = () => {
             <div className={`rounded-br-2xl ${commonStyles}`}>Blockchain</div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
           <div className="p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorpism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
@@ -95,15 +96,17 @@ const Welcome = () => {
             />
              <div className="h-[1px] w-full bg-gray-400 my-2" />
 
+            {
+              false? (<Loader />) :
             
-                <button
+                (<button
                   type="button"
-                  // onClick={handleSubmit}
+                  onClick={handleSubmit}
                   className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
                 >
                   Send now
-                </button>
-              
+                </button>)
+            }
           
           </div>
         </div>
