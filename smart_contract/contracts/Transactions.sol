aa7cd71e96d9580a37ff7bd57bff6b84fa360651
ngirimana <chadrackngirimana@gmail.com>
2022-01-19 21:46:26 +0200

    smart contract in solidity

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.0;
 
 contract Transactions{
-    uint256 traansactionsCount;
-    event Transfer(address from,address receiver,uint amount,string message,uint256 timestamp,string keywaord );
+    uint256 transactionCount;
+    event Transfer(address from,address receiver,uint amount,string message,uint256 timestamp,string keyword );
     struct TransferStruct{
         address sender;
         address receiver;
@@ -16,15 +16,17 @@ contract Transactions{
 
   TransferStruct[]  transactions;
 
-  function addToBlockChain() public {
-    traansactionsCount+=1;
+  function addToBlockChain(address payable reciever,uint amount,string memory message,string memory keyword ) public {
+    transactionCount+=1;
+    transactions.push(TransferStruct(msg.sender,reciever,amount,message,block.timestamp,keyword));
+    emit Transfer(msg.sender,reciever,amount,message,block.timestamp,keyword);
     //1:15:03
   }
 
   function getAllTransactions() public view  returns(TransferStruct[] memory){
-      // return transactions;
+      return transactions;
   }
   function getTransactionCount() public view returns(uint256){
-      // return transactionCount;
+      return transactionCount;
   }
 }
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
+// https://eth-ropsten.alchemyapi.io/v2/xSXv1TOuxC5DjR2uMaVyjqYSwMqBSNTT
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle')
 module.exports = {
-  solidity: "0.8.4",
+  solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: "https://eth-ropsten.alchemyapi.io/v2/xSXv1TOuxC5DjR2uMaVyjqYSwMqBSNTT",
+      accounts: [
+        "2a64276e8d1739dcde972788d53988f8766d338f19c5590f48b0ce86cde53486",
+      ],
+    },
+  },
 };
