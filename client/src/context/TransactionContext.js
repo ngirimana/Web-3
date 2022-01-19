9d11874dda95fa087818f2a79088f22ef01151df
ngirimana <chadrackngirimana@gmail.com>
2022-01-19 22:22:16 +0200

    connect wallet to frontend

---
+++ client/src/context/TransactionContext.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+import { contractABI, contractAddress } from '../utils/constants';
+
+export const TransactionContext = React.createContext()
+
+const { ethereum } = window;
+window.ethereum;
+
+const getEthreumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.signer();
+    const transactionContract = new ethers.Contract(contractAddress,contractABI,signer);
+    console.log({
+        provider, 
+        signer,
+        transactionContract
+    })
+}
+
+export const TransactionProvider = ({children}) => {
+    return (
+        <TransactionContext.Provider>
+            {children}
+        </TransactionContext.Provider>
+    )
+}
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contactAddress = "0x1329b2915D4e1803B3bEebcED3e05EF43124F601";
+export const contractAddress = "0x1329b2915D4e1803B3bEebcED3e05EF43124F601";
